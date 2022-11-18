29186bda37cb7224a1882df636e01eaa5906716e
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-11-18 20:50:07 -0300

    Fix: resolve problemns in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,78 @@
-import { CheckWebGPU } from './helper';
-import shader from './shader.wgsl';
+import { CheckWebGPU } from './helper'; 
+import shader from './shader.wgsl'; // importa shader
 import "./site.css";
 
 const CreateTriangle = async () => {
     const checkgpu = CheckWebGPU();
-    if(checkgpu.includes("Seu browser não suporta WebGPU!")) {
+    if(checkgpu.includes('Your current browser does not support WebGPU!')){
         console.log(checkgpu);
-        throw("Your browser is wrong!");
+        throw('Your current browser does not support WebGPU!');
     }
 
-    // Elemento do canvas
+     // Elemento do canvas
     const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
-
+    
     // WebGPU
-    const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;
+    const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;  
+    // GPUDevice é o objeto que representa a nossa GPU com os métodos de acesso 
     const device = await adapter?.requestDevice() as GPUDevice;
 
     const context = canvas.getContext('webgpu') as GPUCanvasContext;
-    const format = 'bgra8unorm';
-
+    const format = 'bgra8unorm'; 
+    
+    // Configura a WebGPU no Canvas
     context.configure({
         device: device,
-        format: format
+        format: format,
     });
-
+    
+    // https://www.w3.org/TR/webgpu/#gpurenderpipeline
     const pipeline = device.createRenderPipeline({
         vertex: {
-            module: device.createShaderModule({
-                code: shader
+            module: device.createShaderModule({                    
+                code: shader 
             }),
-            entryPoint: "vs_main"
+            entryPoint: "vs_main" 
         },
         fragment: {
-            module: device.createShaderModule({
+            module: device.createShaderModule({                    
                 code: shader
             }),
             entryPoint: "fs_main",
-            targets: [{ format: format}]
+            targets: [{
+                format: format
+            }]
         },
-        primitive: {
-            topology: "triangle-list"
-        }
+        primitive:{
+            topology: "triangle-list" 
+        } 
     });
-
+   
     const commandEncoder = device.createCommandEncoder();
     const textureView = context.getCurrentTexture().createView();
+
+     // Começa o encoding
     const renderPass = commandEncoder.beginRenderPass({
         colorAttachments: [{
             view: textureView,
-            clearValue: {r: 2.0, g: 0.247, b: 0.314, a: 1.0 },
+            clearValue: { r: 0.2, g: 0.247, b: 0.314, a: 1.0 }, 
             loadOp: 'clear',
-            loadValue: {r: 2.0, g: 0.247, b: 0.314, a: 1.0 },
+            loadValue: { r: 0.2, g: 0.247, b: 0.314, a: 1.0 }, 
             storeOp: 'store'
         }]
     });
-
+  
     renderPass.setPipeline(pipeline);
     renderPass.draw(3, 1, 0, 0);
     renderPass.end();
 
     device.queue.submit([commandEncoder.finish()]);
-};
+}
 
+// Executa a função
 CreateTriangle();
+
+// Desenha novamente o triangulo conforme o tamanho da tela
+window.addEventListener('resize', function(){
+    CreateTriangle();
+});
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,35 +1,54 @@
-// Struct (objeto sem métodos) que representa o output
+// shader da vértice
+
+// Output
+// Struct (agrupamento de dados) que representa a saída da função.
+// Propriedades:
+//   - Position do tipo Vetor de 4 floats 32-bit = Posição do elemento
+//   - vColor do tipo Vetor de 4 floats 32-bit = cor v1, v2, v3   
 struct Output {
     @builtin(position) Position : vec4<f32>,
     @location(0) vColor : vec4<f32>
 };
 
-// Main da Vértices 
+// vs_main
+// Função para o Shader das Vértices
+// Retorna: 
+//   - Valor do tipo Output declarado acima.
 @vertex
 fn vs_main(@builtin(vertex_index) VertexIndex: u32) -> Output {
-    // posição que vamos criar
+    // pos é um array de 3 posições do Tipo vec2 de 32-bits 
+    // que representa a posição de 3 vértices do triangulo
     var pos = array<vec2<f32>, 3>(
         vec2<f32>(0.0, 0.5), // Vértice 1
         vec2<f32>(-0.5, -0.5), // Vértice 2
         vec2<f32>(0.5, -0.5)  // Véretice 3
     );
 
-    // cores de cada vértice
+    // color é um array de mesmo tipo que representa as cores 
+    // de cada uma das vértices.
     var color = array<vec3<f32>, 3>(
         vec3<f32>(1.0, 0.0, 0.0), // RED
         vec3<f32>(0.0, 1.0, 0.0), // GREEN
         vec3<f32>(0.0, 0.0, 1.0) // BLUE
     );
 
-    // Criou uma variavel output com a posição e cor
+    // Cria um output
     var output: Output;
+    //   Recebe a posição do elemento com os vértices, mas também os eixos z e w (escala)
+    //   Por isso vec4 = vec2 + z + w
     output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0); 
+    //   Recebe a cor e adiciona 1.0 que é a transparência da cor
     output.vColor = vec4<f32>(color[VertexIndex], 1.0);
 
     return output;
 }
 
-// Main do Fragment
+// shader do fragmento
+
+// fs_main
+// Desestrutura a cor de um output como input e retorna a posição das cores para fazer o gradiente.
+// Recebe:
+//   - vColor que representa os valores de Cor abstraidos como vec4<f32>
 @fragment
 fn fs_main(@location(0) vColor: vec4<f32>) -> @location(0) vec4<f32> {
     return vColor;
